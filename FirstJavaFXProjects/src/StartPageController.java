5f7d10b44554b95341066cfd17abc469410a4dfb
builtbynash <bastinashestrada@gmail.com>
2026-03-23 14:08:14 +0800

    haha

---
+++ FirstJavaFXProjects/src/StartPageController.java
@@ -0,0 +1,25 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import java.io.IOException;
+
+public class StartPageController {
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private void handleStartClick(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("second_page.fxml"));
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
---
+++ FirstJavaFXProjects/src/main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       
+        Parent root = FXMLLoader.load(getClass().getResource("start_page.fxml"));
+        
+        primaryStage.setTitle("CutQueue Barber Shop");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
